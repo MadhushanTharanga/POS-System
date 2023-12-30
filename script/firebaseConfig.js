1219b6cdf7ae6727cc2ac3adb02932db52262137
MadhushanTharanga <madhuwa07@gmail.com>
2023-12-30 22:49:47 +0530

    added firebase for register form

---
+++ script/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDMDSuCUfpg2cx8XDe2P8vYR5qtCOHmmoU",
+    authDomain: "pos-system-29fcb.firebaseapp.com",
+    projectId: "pos-system-29fcb",
+    storageBucket: "pos-system-29fcb.appspot.com",
+    messagingSenderId: "608642946782",
+    appId: "1:608642946782:web:9f65d2cac606bff625544f",
+    measurementId: "G-BQ4WKJZ9ZE"
+  };
+
+  firebase.initializeApp(firebaseConfig);
--- script/register.js
+++ script/register.js
@@ -1,3 +1,23 @@
+const register=()=>{
+
+    const email = $('#email').val();
+    const password = $('#password').val();
+
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email,password)
+    .then((cred)=>{
+        console.log(cred);
+        console.log(cred.user);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+   
+
+}
+
+
 const login=()=>{
     window.location.replace('login.html')
 }
