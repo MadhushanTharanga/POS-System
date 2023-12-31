fd233e4c7110deb47b569f05ab815f1ed845f7fc
MadhushanTharanga <madhuwa07@gmail.com>
2023-12-31 10:13:42 +0530

    added firebase for login form

--- script/login.js
+++ script/login.js
@@ -1,6 +1,18 @@
 
 const login=()=>{
-    window.location.replace('dashboard.html');
+    const email = $('#email').val();
+    const password = $('#password').val();
+
+    firebase
+    .auth()
+    .signInWithEmailAndPassword(email,password)
+    .then((resp)=>{
+        console.log(resp);
+        console.log(resp.user);
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 }
 const register=()=>{
     window.location.replace('register.html');
