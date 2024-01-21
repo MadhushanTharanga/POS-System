8bfd1e8c4e3843ac9b5cb5af63b39a0fd12f6552
MadhushanTharanga <madhuwa07@gmail.com>
2024-01-21 12:07:14 +0530

    updated place order

--- script/placeOrder.js
+++ script/placeOrder.js
@@ -1,4 +1,9 @@
 
+const loadIds = () =>{
+    loadCustomerIds();
+    loadItemIds();
+}
+
 const loadCustomerIds=()=>{
     $('#customer-id').empty();
 
@@ -14,6 +19,21 @@ const loadCustomerIds=()=>{
         }))
 }   
 
+const loadItemIds=()=>{
+    $('#item-id').empty();
+
+    const firestore = firebase.firestore();
+    firestore
+        .collection('items')
+        .get()
+        .then((records=>{
+            records.forEach((result => {
+                const option = $('<option></option>').val(result.id).text(result.id);
+                $('#item-id').append(option);
+            }));
+        }))
+}  
+
 $('#customer-id').on("change" ,function(){
     const customerId=$(this).val();
     const firestore = firebase.firestore();
@@ -27,6 +47,22 @@ $('#customer-id').on("change" ,function(){
                 $('#name').val(data.name);
                 $('#address').val(data.address);
                 $('#salary').val(data.salary)
+            }
+        })
+})
+$('#item-id').on("change" ,function(){
+    const itemId=$(this).val();
+    const firestore = firebase.firestore();
+    firestore
+        .collection('items')
+        .doc(itemId)
+        .get().then((response)=>{
+            
+            if(response.exists){
+                const data = response.data();
+                $('#description').val(data.description);
+                $('#unit-price').val(data.unitPrice);
+                $('#qty-on-hand').val(data.qtyOnHand)
             }
         })
 })
